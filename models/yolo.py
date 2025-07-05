aa1f0db982473acd459f21c19944291cd983cf6f
sigma-chy <2441833953@qq.com>
2025-07-05 15:53:58 +0800

    update

--- models/yolo.py
+++ models/yolo.py
@@ -9,6 +9,7 @@ import torch
 from models.common import *
 from models.experimental import *
 from models.rcsosa import *
+from models.simAM import *
 from utils.autoanchor import check_anchor_order
 from utils.general import make_divisible, check_file, set_logging
 from utils.torch_utils import time_synchronized, fuse_conv_and_bn, model_info, scale_img, initialize_weights, \
